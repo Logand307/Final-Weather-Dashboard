6ae07d3529cfbf80cbcbac334d09acf05312c014
Logan Davis <Ldavis96@live.com>
2021-12-12 21:09:51 -0500

    functions for weather display and card

--- assets/index.js
+++ assets/index.js
@@ -59,3 +59,76 @@ $("#search-btn").on("click", function() {
       </a>`);
     }
 });
+
+$(document).on("click", ".list-group-item", function() {
+    var cityName = $(this).text();
+    clearDisplayedWeatherInfo();
+    resetGlobalVariables();
+    searchCity(cityName);
+});
+
+function displayCurrentWeather() {
+    var cardDiv = $("<div class='container border bg-light'>");
+    var weatherImage = $("<img>").attr('src', currentWeatherIconUrl);
+    var cardHeader = $("<h4>").text(city + " " + currentDate.toString());
+    cardHeader.append(weatherImage);
+    var temperatureEl = $("<p>").text("Temperature: " + tempF + " ºF");
+    var humidityEl = $("<p>").text("Humidity: " + humidityValue + "%");
+    var windSpeedEl = $("<p>").text("Wind Speed: " + windSpeed + " MPH");
+    var uvIndexEl = $("<p>").text("UV Index: ");
+    // var uvIndexValueEl = $("<span>").text(uvIndexValue).css("background-color", getColorCodeForUVIndex(uvIndexValue)).addClass("text-white");
+    var uvIndexValueEl = $("<span>").text(uvIndexValue).css("background-color", getColorCodeForUVIndex(uvIndexValue));
+    uvIndexEl.append(uvIndexValueEl);
+    cardDiv.append(cardHeader);
+    cardDiv.append(temperatureEl);
+    cardDiv.append(humidityEl);
+    cardDiv.append(windSpeedEl);
+    cardDiv.append(uvIndexEl);
+    $("#current-weather-conditions").append(cardDiv);
+}
+
+function displayDayForeCast() {
+    var imgEl = $("<img>").attr("src", iconurl);
+    var cardEl = $("<div class='card'>").addClass("pl-1 bg-primary text-light");
+    var cardBlockDiv = $("<div>").attr("class", "card-block");
+    var cardTitleDiv = $("<div>").attr("class", "card-block");
+    var cardTitleHeader = $("<h6>").text(dateValue).addClass("pt-2");
+    var cardTextDiv = $("<div>").attr("class", "card-text");
+    var minTempEl = $("<p>").text("Min Temp: " + minTempF + " ºF").css("font-size", "0.60rem");
+    var maxTempEl = $("<p>").text("Max Temp: " + maxTempF + " ºF").css("font-size", "0.60rem");
+    var humidityEl = $("<p>").text("Humidity: " + dayhumidity + "%").css("font-size", "0.60rem");
+
+    cardTextDiv.append(imgEl);
+    cardTextDiv.append(minTempEl);
+    cardTextDiv.append(maxTempEl);
+    cardTextDiv.append(humidityEl);
+    cardTitleDiv.append(cardTitleHeader);
+    cardBlockDiv.append(cardTitleDiv);
+    cardBlockDiv.append(cardTextDiv);
+    cardEl.append(cardBlockDiv);
+    $(".card-deck").append(cardEl);
+}
+
+function addCardDeckHeader() {
+    deckHeader = $("<h4>").text("5-Day Forecast").attr("id", "card-deck-title");
+    deckHeader.addClass("pt-4 pt-2");
+    $(".card-deck").before(deckHeader);
+}
+
+function clearDisplayedWeatherInfo() {
+    $("#current-weather-conditions").empty();
+    $("#card-deck-title").remove();
+    $(".card-deck").empty();
+}
+
+function displayCities(citiesList) {
+    $("#searched-cities-card").removeClass("hide");
+    var count = 0;
+    citiesList.length > 5 ? count = 5 : count = citiesList.length
+    for (var i = 0; i < count; i++) {
+        $("#searched-cities-list").css("list-style-type", "none");
+        $("#searched-cities-list").append(`<a href="#" class="list-group-item" style="text-decoration: none; color: black;">
+      <li>${citiesList[i]}</li>
+      </a>`);
+    }
+}
