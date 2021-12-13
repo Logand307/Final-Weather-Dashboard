5e05d12248409a05f612e6e94d002e7fa375bcf1
Logan Davis <Ldavis96@live.com>
2021-12-12 21:06:38 -0500

    added javascript variables

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+var APIKey = "2dd64feb8132b85a8a95f90d2cff6658";
+var city = "";
+var currentDate = "";
+var tempF = "";
+var humidityValue = "";
+var windSpeed = "";
+var uvIndexValue = "";
+var latitude = "";
+var longitude = "";
+var minTempK = "";
+var maxTempK = "";
+var minTempF = "";
+var maxTempF = "";
+var dayhumidity = "";
+var currentWeatherIconCode = "";
+var currentWeatherIconUrl = "";
+var iconcode = "";
+var iconurl = "";
+var country = "";
+
+var listOfSearchedCities = [];
+
+var getSeachedCitiesFromLS = JSON.parse(localStorage.getItem("searched-cities"));
